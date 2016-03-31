c526848c4e2fe3c5c98e657a4bf5d995ca8d9ddd
syo-sa1982 <fujii1308@gmail.com>
2016-03-31 14:41:43 +0900

    試しに書いたけどlocalhostが拒否られる件

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/engine/standard"
+	"fmt"
+)
+
+func hello() echo.HandlerFunc  {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	}
+}
+
+func main() {
+	fmt.Println("main")
+	// Echo instance
+	e := echo.New()
+	fmt.Println(e)
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.Get("/", hello())
+
+	fmt.Println("Server running at http://localhost:1323")
+	// Start server
+	e.Run(standard.New(":1323"))
+}
