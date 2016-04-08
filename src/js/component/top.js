7e03b86c5bb0f8280831b2c7775d0104760af86e
sairoutine <sairoutine@gmail.com>
2016-04-09 01:23:47 +0900

    Add: atnd top page

--- src/js/component/top.js
+++ src/js/component/top.js
@@ -7,7 +7,7 @@
 
 var m = require('../mithril');
 
-// ナビバー
+// navbar
 var Navbar = require('./navbar');
 
 
@@ -16,10 +16,27 @@ module.exports = {
 	},
 	view: function(ctrl) {
 		return <div>
+			{/*navbar*/}
 			<div>{ m.component(Navbar) }</div>
 
+			{/*jumbotron*/}
 			<div class="container" style="padding-top:30px" id="root">
-				Hello World!
+				<div class="jumbotron" style="background-color: rgba(255, 255, 255, 0.7);">
+					<div class="container">
+						<h1>Go ATND</h1>
+						<p>
+							イベントの告知・運営管理がすべて無料で出来ます。<br />
+							仲間内の飲み会から勉強会まで<br />
+							色んなイベントで色んな人に会いましょう。<br />
+						</p>
+						<p>
+							<a class="btn btn-info btn-lg" href="/event/create" config={m.route} role="button">
+								<span class="glyphicon glyphicon-log-in">　</span>
+								新しいイベントを作る
+							</a>
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>;
 	}
