41c14c08abfe5bb0ee2e8e26f2a32fa3fca21c13
sairoutine <sairoutine@gmail.com>
2016-04-09 01:24:47 +0900

    Add: top & event/create & event/list routings

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,18 @@ var m = require('./mithril');
 // TOPページ
 var Top = require('./component/top.js');
 
+// イベント作成ページ
+var EventCreate = require('./component/event/create.js');
+// イベント一覧ページ
+var EventList = require('./component/event/list.js');
+
+
+
+
+
 //HTML要素にコンポーネントをマウント
 m.route(document.getElementById("root"), "/", {
 	"/": Top,
+	"/event/create": EventCreate,
+	"/event": EventList,
 });
