0dca2adefda08c9e3fa43c5cbe1d3b8947bda02a
sairoutine <sairoutine@gmail.com>
2016-04-09 01:24:03 +0900

    Add: atnd navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,24 +8,26 @@ module.exports = {
 		var active_url = m.route();
 
 		return <div>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar">&nbsp;</span>
-						<span class="icon-bar">&nbsp;</span>
-						<span class="icon-bar">&nbsp;</span>
-					</button>
-					<span class="navbar-brand">Go ATND</span>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar">&nbsp;</span>
+							<span class="icon-bar">&nbsp;</span>
+							<span class="icon-bar">&nbsp;</span>
+						</button>
+						<span class="navbar-brand">Go ATND</span>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class={ active_url === "/" ? "active" : "" }><a href="/" config={m.route}>TOP</a></li>
+							<li class={ active_url === "/event" ? "active" : "" }><a href="/event" config={m.route}>イベント一覧</a></li>
+							<li class={ active_url === "/event/create" ? "active" : "" }><a href="/event/create" config={m.route}>新しくイベントを作る</a></li>
+						</ul>
+					</div>
 				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class={ active_url === "/" ? "active" : "" }><a href="/" config={m.route}>TOP</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+			</nav>
 		</div>;
 	}
 };
