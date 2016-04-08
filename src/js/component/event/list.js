84803f9662405c97f28960a4deed8f29919e95b0
sairoutine <sairoutine@gmail.com>
2016-04-09 01:50:40 +0900

    Add: event/list mock page

--- src/js/component/event/list.js
+++ src/js/component/event/list.js
@@ -21,7 +21,46 @@ module.exports = {
 
 			{/*jumbotron*/}
 			<div class="container" style="padding-top:30px" id="root">
-				イベント一覧ページ
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<a href="/event/1" config={m.route}>
+							イベント名1
+						</a>
+					</div>
+					<div class="panel-body">
+						<div class="pull-left">
+							<img src="img/150x150.png" height="150" width="150" />
+						</div>
+						イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細
+					</div>
+					<div class="panel-footer">
+						<div class="pull-right">
+							2016/04/09 (土) 13:30 から
+						</div>
+							225/250人
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<a href="/event/1" config={m.route}>
+							イベント名1
+						</a>
+
+					</div>
+					<div class="panel-body">
+						<div class="pull-left">
+							<img src="img/150x150.png" height="150" width="150" />
+						</div>
+						イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細
+					</div>
+					<div class="panel-footer">
+						<div class="pull-right">
+							2016/04/09 (土) 13:30 から
+						</div>
+							225/250人
+					</div>
+				</div>
 			</div>
 		</div>;
 	}
