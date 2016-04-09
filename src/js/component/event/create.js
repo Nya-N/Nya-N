3d8d0627f236c04f61263a4b002423a9a60256cb
sairoutine <sairoutine@gmail.com>
2016-04-09 12:53:03 +0900

    Modify: brush up event/create page to show confirm modal

--- src/js/component/event/create.js
+++ src/js/component/event/create.js
@@ -13,6 +13,15 @@ var Navbar = require('../navbar');
 
 module.exports = {
 	controller: function() {
+		// イベント作成ボタンが押下された時
+		this.onsubmit = function(e) {
+			e.preventDefault();
+
+			// TODO: イベント登録処理
+
+			// イベント詳細画面に遷移
+			m.route('/event/detail/1');
+		};
 	},
 	view: function(ctrl) {
 		return <div>
@@ -57,7 +66,29 @@ module.exports = {
 				</div>
 
 				<div>
-					<button type="submit" class="btn btn-lg btn-success">イベントを新規作成</button>
+					<button type="button" class="btn btn-lg btn-success" data-toggle="modal" data-target="#ConfirmModal">イベントを新規作成</button>
+				</div>
+
+				{/* 確認画面モーダル */}
+				<div id="ConfirmModal" class="modal fade" role="dialog">
+					<div class="modal-dialog">
+
+						<div class="modal-content">
+							<div class="modal-header">
+								{/* 閉じるボタン */}
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">確認画面</h4>
+							</div>
+							<div class="modal-body">
+								<p>{/* TODO: 確認事項を書く */}</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-lg btn-success" data-dismiss="modal"  onclick={ctrl.onsubmit}>送信</button>
+								<button type="button" class="btn btn-lg btn-warning" data-dismiss="modal">修正</button>
+							</div>
+						</div>
+
+					</div>
 				</div>
 			</form>
 
