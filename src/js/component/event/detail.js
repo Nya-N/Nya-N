82e79a1416788404a40c41acb36c107d0d1cf630
sairoutine <sairoutine@gmail.com>
2016-04-09 19:06:48 +0900

    Modify: to see api data in event/detail page

--- src/js/component/event/detail.js
+++ src/js/component/event/detail.js
@@ -10,22 +10,32 @@ var m = require('../../mithril');
 // navbar
 var Navbar = require('../navbar');
 
+// アプリケーションの状態
+var state = require('../../state');
 
 module.exports = {
 	controller: function() {
 		// イベントID
 		this.id = m.route.param("id");
+
+		// TODO: IDが存在しなかった場合のエラー処理
+
+		// ViewModel
+		this.vm = state.make_event_detail(this.id);
 	},
 	view: function(ctrl) {
+		var model = ctrl.vm.model();
+
+		// HTML
 		return <div>
 			{/*navbar*/}
 			<div>{ m.component(Navbar) }</div>
 
 			<div class="container" style="padding-top:30px" id="root">
-				{/* イベント名 */}
 				<div class="row">
 					<div class="col-md-12">
-						<h1>イベント1</h1>
+						{/* イベント名 */}
+						<h1>{model.name}</h1>
 					</div>
 				</div>
 
@@ -38,15 +48,15 @@ module.exports = {
 							<tbody>
 								<tr>
 									<td>日時</td>
-									<td>2016/04/09 (土) 13:30 から</td>
+									<td>{ model.start_date + "から"}</td>
 								</tr>
 								<tr>
 									<td>主催者</td>
-									<td>田中</td>
+									<td>{model.admin.name}</td>
 								</tr>
 								<tr>
 									<td>開催場所</td>
-									<td>第一会議室</td>
+									<td>{model.place.name}</td>
 								</tr>
 							</tbody>
 						</table>
@@ -54,67 +64,26 @@ module.exports = {
 						<div class="panel panel-default">
 							<div class="panel-body">
 								{/* イベント詳細 */}
-								<h2>勉強会の趣旨</h2>
-								<p>Go言語をプロダクションで導入している企業による勉強会です。<br />
-								導入の事例紹介やノウハウ共有ができる場にしたいと思います。</p>
-								<h2>タイムテーブル</h2>
-								<table class="table">
-								<tr>
-									<th>時間</th>
-									<th>内容</th>
-									<th>発表者</th>
-								</tr>
-								<tr>
-									<td>19:00 – 19:20</td>
-									<td>開場</td>
-									<td>-</td>
-								</tr>
-								<tr>
-									<td>19:20 – 19:30</td>
-									<td>挨拶＆会場説明</td>
-									<td>-</td>
-								</tr>
-								<tr>
-									<td>19:30 – 19:40</td>
-									<td>自己紹介タイム</td>
-									<td>参加者全員</td>
-								</tr>
-								<tr>
-									<td>19:40 – 20:00</td>
-									<td>ああああああああああについて</td>
-									<td>株式会社ああああああああ</td>
-								</tr>
-								<tr>
-									<td>20:00 – 20:20</td>
-									<td>ああああああああああについて</td>
-									<td>株式会社ああああああああ</td>
-								</tr>
-								<tr>
-									<td>20:20 – 20:40</td>
-									<td>ああああああああああについて</td>
-									<td>株式会社ああああああああ</td>
-								</tr>
-								<tr>
-									<td>20:40 – 21:00</td>
-									<td>質疑応答タイム</td>
-									<td>-</td>
-								</tr>
-								</table>
-								<h2>勉強会の対象者</h2>
-								<p>Go言語に興味があるエンジニアのかた<br />
-								これからGo言語の導入を検討されているかた</p>
+								{ m.trust(model.description) }
 							</div>
 						</div>
 
 						<div class="panel panel-default">
 							<div class="panel-heading">
-								コメント一覧(1)
+								{/* コメント数 */}
+								コメント一覧({ model.comment_num })
 							</div>
 							<div class="panel-body">
 								{/* コメント一覧 */}
-								山本さん<br />
-								よろしくお願いします！
-								<hr />
+								{
+									model.comments.map(function(comment, i) {
+										return <div>
+											{/* コメント投稿者 */}
+											{ comment.name }<br />
+											{ comment.comment }<hr />
+										</div>;
+									})
+								}
 								{/* コメント投稿フォーム */}
 								<form>
 									<div class="form-group">
@@ -134,22 +103,21 @@ module.exports = {
 						<button type="button" class="btn btn-lg btn-success" data-toggle="modal" data-target="#AttendModal">
 							イベントに参加する
 						</button>
-						<h3>参加人数 100 / 225</h3>
+						<h3>参加人数 {model.attend_num} / {model.capacity}</h3>
 
 						<div class="panel panel-default">
 							<div class="panel-heading">
-									参加者一覧
+									{/* 参加者数 */}
+									参加者一覧({model.member_num})
 							</div>
 							<div class="panel-body">
-								山本 さん<br />
-								田中 さん<br />
-								太郎 さん<br />
-								山本 さん<br />
-								田中 さん<br />
-								太郎 さん<br />
-								山本 さん<br />
-								田中 さん<br />
-								太郎 さん<br />
+								{
+									model.members.map(function(member, i) {
+										return <span>
+											{ member.name } さん<br />
+										</span>;
+									})
+								}
 							</div>
 						</div>
 					</div>
