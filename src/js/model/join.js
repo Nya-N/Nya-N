7805f5339c0017d7baf5e133303dc5e011f03b6e
sairoutine <sairoutine@gmail.com>
2016-04-10 01:55:37 +0900

    Add: model class of join

---
+++ src/js/model/join.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*
+ * イベントに付随する参加者 モデル
+ *
+ */
+
+// API URL
+var api_url = "api/join";
+
+
+var m = require('../mithril');
+
+
+// コンストラクタ
+var Model = function (data, isInitial) {
+	if( ! data) {
+		data = {};
+	}
+	// JOIN ID
+	this.id = m.prop(data.id);
+	// イベントID
+	this.event_id = m.prop(data.event_id);
+	// 参加者の名前
+	this.name = m.prop(data.name || "");
+};
+
+// サーバからJSONを読み込む
+Model.read = function (id) {
+	return m.request({
+		method: "GET",
+		url: api_url + '/' + id,
+		type: Model
+	});
+};
+
+// サーバにJSONを保存
+Model.prototype.save = function () {
+	return m.request({method: "POST", url: api_url, data: {
+		event_id: this.event_id(),
+		name:     this.name(),
+	}})
+	.then(function(res) {
+		// 生成されたコメントID
+		return res.id;
+	});
+};
+
+module.exports = Model;
+
