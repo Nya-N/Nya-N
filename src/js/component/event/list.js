e34a2dfc31cf80ae1164ae789170d2c9efc9c755
sairoutine <sairoutine@gmail.com>
2016-04-09 16:49:23 +0900

    Modify: pager to use model property

--- src/js/component/event/list.js
+++ src/js/component/event/list.js
@@ -5,16 +5,28 @@
  *
  */
 
+// mithril
 var m = require('../../mithril');
+
+// アプリケーションの状態
+var state = require('../../state');
 
 // navbar
 var Navbar = require('../navbar');
 
-
 module.exports = {
 	controller: function() {
+		// ViewModel
+		this.vm = state.make_event_list();
 	},
 	view: function(ctrl) {
+		var model = ctrl.vm.model();
+
+		// 前へ
+		var prev_id = model.prev_id;
+		// 次へ
+		var next_id = model.next_id;
+
 		return <div>
 			{/*navbar*/}
 			<div>{ m.component(Navbar) }</div>
@@ -62,10 +74,11 @@ module.exports = {
 				</div>
 				{/* ページャー */}
 				<nav>
-				  <ul class="pager">
-					<li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
-					<li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
-				  </ul>
+					<ul class="pager">
+				  		{/*<li class="previous disabled"><a href=""><span aria-hidden="true">&larr;</span> Older</a></li>*/}
+				  		<li class="previous"><a href={"/event?prev_id=" + prev_id} config={m.route}><span aria-hidden="true">&larr;</span> Older</a></li>
+						<li class="next"><a href={"/event?next_id=" + next_id} config={m.route}>Newer <span aria-hidden="true">&rarr;</span></a></li>
+				  	</ul>
 				</nav>
 			</div>
 		</div>;
