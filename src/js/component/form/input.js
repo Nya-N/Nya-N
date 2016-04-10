e7c4e1a61a295dc83d07c4f06d9359853f6d2eea
sairoutine <sairoutine@gmail.com>
2016-04-10 20:29:42 +0900

    Add: form input component

---
+++ src/js/component/form/input.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+ * input タグ
+ *
+ */
+
+var m = require('../../mithril');
+
+module.exports = {
+	controller: function() {},
+	view: function(ctrl, args) {
+		// プロパティ
+		var prop = args.prop;
+		// エラーメッセージ
+		var error = args.error;
+		// placeholder
+		var placeholder = args.placeholder;
+
+		return <div class={ error ? "form-group has-error has-feedback" : "form-group"}>
+			<input type="text" class="form-control" placeholder={ placeholder } onchange={m.withAttr("value", prop)} value={ prop() } />
+			{ error ? <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span> : "" }
+			<span class="help-block">{ error }</span>
+		</div>;
+	}
+};
