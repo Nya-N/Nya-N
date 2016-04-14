f74db7dd17305b576a0cd331e1e973cc77ca8bad
syo-sa1982 <fujii1308@gmail.com>
2016-04-14 17:49:52 +0900

    マイグレーションファイルの修正 テーブルのエンコードが何故かutf8じゃないので、修正 テスト用の仮データ

--- db/migrate.go
+++ db/migrate.go
@@ -7,7 +7,28 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"time"
 )
+
+var Events = []model.Event{
+	model.Event{
+		ID: 1,
+		Name: "イベント名1",
+		ImagePath: "img/150x150.png",
+		Capacity: 250,
+		StartDate: time.Now(),
+		Description: "イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細イベント1の詳細",
+	},
+	model.Event{
+		ID: 2,
+		Name: "イベント名2",
+		ImagePath: "img/150x150.png",
+		Capacity: 250,
+		StartDate: time.Now(),
+		Description: "イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細イベント2の詳細",
+	},
+}
+
 
 func main() {
 	yml, err := ioutil.ReadFile("conf/db.yaml")
@@ -30,11 +51,18 @@ func main() {
 	if db.HasTable(&model.Member{}) {
 		db.DropTable(&model.Member{})
 	}
-	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.Member{})
+	db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").CreateTable(&model.Member{})
 
 	log.Println("Event")
 	if db.HasTable(&model.Event{}) {
 		db.DropTable(&model.Event{})
 	}
-	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.Event{})
+	db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").CreateTable(&model.Event{})
+
+	log.Println("SkillCategory INSERT")
+	for key := range Events {
+		db.Create(&Events[key])
+	}
+	
+	
 }
