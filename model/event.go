d2a32908ac18fd8ba7dfafc3825c22cb81c7bd8d
syo-sa1982 <fujii1308@gmail.com>
2016-04-14 17:51:20 +0900

    モデルの各項目を下記に合わせる https://github.com/syo-sa1982/GoNTAkun/blob/master/public/api/event_get.json

--- model/event.go
+++ model/event.go
@@ -3,15 +3,17 @@ import "time"
 
 type Event struct {
 	ID int
-	Title string `sql:"size:255"`
-	ImageName string `sql:"size:255"`
-	MaxMember int
+	Name string `sql:"size:255"`
+	ImagePath string `sql:"size:255"`
+	Capacity int
 	Members []Member
+	StartDate time.Time
 	Place string `sql:"size:255"`
 	Description string `sql:"size:255"`
-	Comment string // ここだけ後で考える
+	Comment string `sql:"size:255"`// ここだけ後で考える
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time
 }
+
