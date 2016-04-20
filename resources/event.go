7673dfb884d4f98180b69c23309751db5a3177c0
syo-sa1982 <fujii1308@gmail.com>
2016-04-20 09:21:21 +0900

    イベント一覧Done.

--- resources/event.go
+++ resources/event.go
@@ -14,26 +14,30 @@ func (resource *Resource) GetEvents() echo.HandlerFunc {
 		var (
 			db = resource.DB
 			events = []model.Event{}
+			viewCount = 3
+			current int
+			prev_id int
+			next_id int
 		)
-		prev_id, _ := strconv.Atoi(c.QueryParam("prev_id"))
-		next_id, _ := strconv.Atoi(c.QueryParam("next_id"))
+		if (c.QueryParam("current") != "") {
+			current, _ = strconv.Atoi(c.QueryParam("current"))
+		} else {
+			current = 1
+		}
+		prev_id = current - 1
+		next_id = current + 1
 
-		db.Model(events).Find(&events)
+
+		db.Model(events).Offset((current - 1) * 3).Limit(viewCount).Find(&events)
 
 		log.Println(events)
 		log.Println(prev_id)
 		log.Println(next_id)
 
-		api := EventListAPI{
-			prev_id,
-			next_id,
-			events,
-		}
+		response := EventListAPI{ prev_id, next_id, events}
+		api := APIFormat{"success", 1, 0, response}
+
 		log.Println(api)
-		//
-		//	if err := c.Bind(api); err != nil {
-		//		return err
-		//	}
 
 		return c.JSON(http.StatusOK, &api)
 	}
