c1f30fdef5ab1330a0e52bf53022b3382a95ba37
sairoutine <sairoutine@gmail.com>
2016-04-26 20:51:13 +0900

    revert ./resources -> github.com/syo-sa1982/GoNTAkun/resources

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/engine/standard"
 	"fmt"
 	"github.com/jinzhu/gorm"
-	"./resources"
+	"github.com/syo-sa1982/GoNTAkun/resources"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"os"
