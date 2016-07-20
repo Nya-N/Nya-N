39015fc8d234a67c6979fe3630b68065a9db57f0
konaka-h <kona5722@gammail.com>
2016-07-20 11:57:23 +0000

    Googleのプロフィールを構造体に格納するように

--- resources/login.go
+++ resources/login.go
@@ -9,37 +9,35 @@ import (
 	"golang.org/x/oauth2"
 	"github.com/syo-sa1982/GoNTAkun/model"
 	"encoding/json"
-	"reflect"
 )
 
 func (resource *Resource) GetLogin() echo.HandlerFunc {
 
 	return func(c echo.Context) error {
 
-		// ここから　google認証*******
-		fmt.Println("Google認証処理を開始します")
+		// TODO:クッキーをみてIDを取得して、DBからIDをキーにトークンを取得する、トークンがない、もしくは有効期限切れてるならGoogle認証
+		// 有効期限切れはもう少し簡単な方法があったきがする・・・
+
+		// Google認証処理を開始
+		fmt.Println("Google認証処理を開始")
 		// JSONから設定ファイル読み込み
 		confFile, err := ioutil.ReadFile("resources/client_secret.json")
 		if err != nil {
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
-		fmt.Println("JSON読み込み成功")
+		// JSONからconfig生成
 		config, err := google.ConfigFromJSON(confFile)
 		if err != nil {
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
+		// プロフィール情報を使いますよっとスコープ宣言 TODO:カレンダー使うなら追加する GoogleDeveloperCenterから使用APIの設定も忘れずに
 		config.Scopes = []string{
 			"https://www.googleapis.com/auth/userinfo.profile",
 		}
-		fmt.Println("JSONからconfigの生成成功")
 		authURL := config.AuthCodeURL("state")
 		fmt.Println("URL取得。URL= ", authURL)
-		fmt.Println("リダイレクトする。リダイレクトの仕方がわからん！！！")
+		// 認証ページにリダイレクト
 		return c.Redirect(http.StatusMovedPermanently,authURL)
-		//return c.Redirect(305,authURL)
-		// ここまで  google認証*******
-
-
 	}
 }
 
@@ -53,14 +51,15 @@ func (resource *Resource) GetOauth() echo.HandlerFunc {
 		if err != nil {
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
-		fmt.Println("JSON読み込み成功")
+		// JSONからconfig生成
 		config, err := google.ConfigFromJSON(confFile)
 		if err != nil {
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
+		// googleからもらったcodeパラメータからトークンを生成する
 		token, err := config.Exchange(oauth2.NoContext, c.QueryParam("code"))
 		if err != nil {
-			log.Fatalf("Unable to read client secret file: %v", err)
+			log.Fatalf("Ecchange token by code : %v", err)
 		}
 		fmt.Println("token= ", token)
 
@@ -72,32 +71,16 @@ func (resource *Resource) GetOauth() echo.HandlerFunc {
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
 
-		// JSONでプロフィールを取得
-		buf := make([]byte, 1024)
-		res.Body.Read(buf)
-		fmt.Println("body= ", string(buf))
-		var test string
-		test = string(buf)
-		fmt.Println("test= ", test)
-		fmt.Println(reflect.TypeOf(test))
+		// JSONからプロフィールを構造体に変換
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("Unable to read res body: %v", err)
+		}
 		var auth model.Auth
-		json.Unmarshal([]byte(test), &auth)
+		json.Unmarshal(b, &auth)
 		fmt.Printf("contents= %#v\n", auth)
 
-		//response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-		//
-		//defer response.Body.Close()
-		//contents, err := ioutil.ReadAll(response.Body)
-		//fmt.Println("contents= ", contents)
-
-		//fmt.Println("res= ", res)
-		//fmt.Println("res.Body= ", res.Body)
-
-
-
 		return nil
-		// ここまで  google認証*******
-
 
 	}
 }
