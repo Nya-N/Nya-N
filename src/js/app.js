375c17d997319bb11db0f6f4f2d9b4317effbf81
syo-sa1982 <fujii1308@gmail.com>
2016-07-25 18:05:05 +0900

    Cookieの取得をJSに追加

--- src/js/app.js
+++ src/js/app.js
@@ -14,9 +14,22 @@ var EventDetail = require('./component/event/detail.js');
 // イベント編集ページ
 var EventEdit = require('./component/event/edit.js');
 
+var AccountID = getCookie("id");
 
-
-
+function getCookie(c_name){
+	var st="",
+		ed="";
+	if (document.cookie.length>0) {
+		st = document.cookie.indexOf(c_name + "=");
+		if (st != -1) {
+			st = st+c_name.length+1;
+			ed = document.cookie.indexOf(";",st);
+			if (ed == -1) { ed = document.cookie.length };
+			return unescape(document.cookie.substring(st,ed));
+		}
+	}
+	return "";
+}
 
 m.route.mode = "hash";
 
