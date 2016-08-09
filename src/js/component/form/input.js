5bb56c27b28998bd94ab6ff33efce6ae11a0e07f
Keisuke Mori <keisuke.a.mori@o-04975-mac.local>
2016-08-09 12:00:48 +0900

    fix PR review1

--- src/js/component/form/input.js
+++ src/js/component/form/input.js
@@ -19,17 +19,17 @@ module.exports = {
 		// onfocus
 		var onfocus = args.onfocus;
 
-		var element = { tag: "input", attrs: {
-						type: "text",
-						class: "form-control",
-						placeholder: placeholder ,
-						onchange: m.withAttr("value", prop),
-						value: prop(),
-						config: args.config
-					}};
+		var inputElement = m("input", {
+					type: "text",
+					class: "form-control",
+					placeholder: placeholder,
+					onchange: m.withAttr("value", prop),
+					value: prop(),
+					config: args.config
+				});
 
 		return <div class={ error ? "form-group has-error has-feedback" : "form-group"}>
-		  { element }
+		  	{ inputElement }
 			{ error ? <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span> : "" }
 			<span class="help-block">{ error }</span>
 		</div>;
